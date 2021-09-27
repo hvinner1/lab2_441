740f28c82562249238a89ebafc2342d706ab4469
hvinner <91498701+hvinner1@users.noreply.github.com>
2021-09-27 20:54:12 +0000

    change location of stop

--- main.py
+++ main.py
@@ -39,7 +39,8 @@ try:
         pgreen.ChangeDutyCycle(dc)
       for dc in range(101, 0, -1):
         pgreen.ChangeDutyCycle(dc)
-
+    pred.stop()
+    pgreen.stop()
 
 
   GPIO.add_event_detect(b1, GPIO.RISING, callback=myCallback,
@@ -65,23 +66,3 @@ pwm.stop()
 GPIO.cleanup()
 #switch 1 pressed
 
-
-'''
-When switch #1 is pressed, LED #1 produces a single cycle of a 1 Hz triangle waveform
-(smooth transition from low à high à low).
-– When switch #2 is pressed, LED #2 produces a single cycle of a 1 Hz triangle waveform.
-– LED #3 continually blinks on/off at 1 Hz, independent other actions.
-– Use exception handling to exit elegantly on ctrl-C by cleaning up the GPIO ports.
-'''
-
-
-'''while True: # continuous loop
-  if GPIO.input(b1) == GPIO.HIGH:
-        print("Button was pushed!")
-
-
-
-  GPIO.output(white, 0) # set output to 0V
-  sleep(0.5) # wait 0.5 sec
-  GPIO.output(white, 1) # set output to 3.3V
-  sleep(0.5) # wait 0.5 sec'''
